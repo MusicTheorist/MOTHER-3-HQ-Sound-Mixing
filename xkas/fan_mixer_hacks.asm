11082d934274e80bae89cd0d8bf60b02d47b2e5b
MusicTheorist <25316286+MusicTheorist@users.noreply.github.com>
2023-05-14 17:52:48 -0400

    improve track player documentation

--- xkas/fan_mixer_hacks.asm
+++ xkas/fan_mixer_hacks.asm
@@ -32,10 +32,11 @@ org $8069314; dd $2005230
 org $8069434; dd $2005230
 
 // Step #3: time to make room for our new mixer and buffer! Move a handful of Mother 3's various music/sound effects arrays to where
-//          battle RNG used to be in EWRAM.
-org $8120E44; dd $2001090	// 0x140 bytes, used for door knocks
-org $8120E68; dd $20011D0	// 0x500 bytes, used for voice samples
-org $8120E8C; dd $20016D0	// 0x320 bytes, used for chapter themes
+//          battle RNG used to be in EWRAM (none of the battle-related players were moved to EWRAM out of fear of lagging/messing with
+//          Sound Battles; EWRAM is slower than IWRAM, after all!)
+org $8120E44; dd $2001090	// track player  #4, 0x140 bytes        -- used for door knocks and, most importantly, Alec's fart in Chapter 1
+org $8120E68; dd $20011D0	// track player  #7, 0x500 bytes (whoa) -- used for voice samples
+org $8120E8C; dd $20016D0	// track player #10, 0x320 bytes        -- used for chapter themes
 
 // Step #4: move Mother 3's large "main sound area" struct to an earlier position in IWRAM (Kura. and theo3 call it "gSoundInfo")
 org $808F9F8; dd $3000C50
@@ -58,15 +59,15 @@ org $808ED70; dd $3001C01
 //          of the main music player arrays, causing the game to not load all instruments and screw up tempo changes during many songs...
 //          nice of our earlier memory management giving us even more room after the mixing code! We'll move both main music arrays, just
 //          for the sake of organization.
-org $8120E20; dd $3002490
-org $8120E2C; dd $30027B0
+org $8120E20; dd $3002490   // track player #1, 0x320 bytes -- the "default" music player; primarily used for overworld music
+org $8120E2C; dd $30027B0   // track player #2, 0x320 bytes -- used for songs that interrupt the overworld music (examples include battles, menus, and saving)
 
 // Step #10: last thing we need to do! Time to put the rest of Mother 3's music player arrays into place...
-org $8120E38; dd $3000930	// 0x0A0 bytes, used for battle SFX
-org $8120E50; dd $3000570	// 0x280 bytes, used for explosions
-org $8120E5C; dd $30007F0	// 0x140 bytes, used for menu cursor/text printing
-org $8120E74; dd $30009D0	// 0x280 bytes, used for combo instruments
-org $8120E80; dd $30004D0	// 0x0A0 bytes, used for monster noises
+org $8120E38; dd $3000930	// track player #3, 0x0A0 bytes -- used for battle SFX (*not* including combo instruments)
+org $8120E50; dd $3000570	// track player #5, 0x280 bytes -- used for explosions
+org $8120E5C; dd $30007F0	// track player #6, 0x140 bytes -- used for menu cursors and text printing
+org $8120E74; dd $30009D0	// track player #8, 0x280 bytes -- used for combo instruments
+org $8120E80; dd $30004D0	// track player #9, 0x0A0 bytes -- used for monster noises
 
 // Optional step: you can change the sample rate of the game's audio by uncommenting the next org and substituting 'x' with any number below:
 // org $808FA02; db $9x
